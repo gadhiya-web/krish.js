a268d0dc5894f13c045cc86924b7ff680e0c3457
gadhiya-web <kgadhiya97@gmail.com>
2024-11-14 10:52:45 +0000

    new file

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+// LETCHUR =>5 START//
+
+"use strict"; //treat all js code as newer version
+
+// alert("hello")  // we are using nodejs not browser
+
+// console.log(3 + 3);  
+// console.log(krish);
+
+
+let name = "krish"
+let age = 18
+let isloggedin = false
+let state;
+ /*
+// data type //
+null (output => object)
+ number => 2 power 53
+ bigint
+ string => ""
+ boolen => true / falce
+ null => standallone value
+ undefined (output undefine)
+ symbol
+ object
+ */
+
+ console.log(typeof null);
+ // LETCHUR =>5 END//
