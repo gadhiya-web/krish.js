089d0ea82b84d13dbae67d773b1a42b1a6aabc8c
gadhiya-web <kgadhiya97@gmail.com>
2024-11-14 08:47:57 +0000

    LETCHUR-4 start in js

---
+++ 01_basics/01_veriable.js
@@ -0,0 +1,36 @@
+// letchur-4 start//
+
+const accountId = 11223344
+let accountEmail = "krish@gmail.com"
+var accountPassword = "12345"
+accountCity = "kagvad"
+let accountState;
+
+// accountid = 2 //not allowed
+
+accountEmail = "krish@gmail.com"
+accountPassword = "12345"
+accountCity = "jetpur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+//example-start//
+/*  
+accountId = 11223344
+accountEmail = "krish@gmail.com"
+accountPassword = "12121212"
+accountCity = "kagvd"
+
+console.table ([accountId, accountEmail, accountPassword, accountCity])
+*/
+//example-end//
+
+//only for use in a (let)//
+
+// not use is a /* var */ //
+
+// letchur-4 end //
+
