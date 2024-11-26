9f17114f104d9c67d2bfe2974c72478502c86039
gadhiya krish <kgadhiya97@gmail.com>
2024-11-26 05:37:09 +0000

    new file

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -30,12 +30,13 @@ console.log(Math);
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 9, 3, 5));
+// console.log(Math.max(4, 9, 3, 5));
 
 // console.log(Math.random());
 // console.log((Math.random()*10) + 1);
 // console.log(Math.floor(Math.random()*10) + 1);
 
-const min = 10 
+const min = 10
 const max = 20 
 
 // console.log(Math.floor(Math.random() * (max - min + 1)) + min);
