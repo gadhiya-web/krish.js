2ad6662008c0ea5f75eee08846326785755df5a7
gadhiya <kgadhiya97@gmail.com>
2024-11-29 14:11:36 +0530

    add a new file

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -7,35 +7,36 @@ const myheors = ["shaktiman", "nagaraj"]
 
 
 
-const myArr = new Array(1, 2, 3, 4)
+// const myArr = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
 //array methods
 
-// const myArr = [1, 2, 3, 4];
+const myArr = [0, 1, 2, 3, 4];
 
 
 myArr.push(6)
 myArr.push(7)
 myArr.pop()
-console.log(myArr);
+// console.log(myArr);
 
 
 myArr.unshift(9)
 myArr.shift()
-console.log(myArr);
+// console.log(myArr);
 
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(4));
-console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+// console.log(myArr);
 
 
 
 
 const newArr = myArr.join();
-console.log(myArr);
-console.log(typeof newArr);
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
 
 
 /* ++++++++++++++++++++  slice, splice  +++++++++++++++ */
@@ -44,8 +45,8 @@ console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log("B ", myArr);
 console.log(myn1);
+console.log("B ", myArr);
 
 const myn2 = myArr.splice(1, 3)
 console.log("c ", myArr);
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,4 @@
+//  singalton
+
+//  object literals
+
