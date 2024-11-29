4d704d38de305b9f5e58dcbb05136761f01943fc
gadhiya <kgadhiya97@gmail.com>
2024-11-29 15:54:42 +0530

    add a new file

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -2,25 +2,28 @@ const marval_heros = ["thor", "ironmen", "spidermen"]
 const dc_heros = ["supermen", "flace", "betmen"]
 
 
-// i like this method in array
+// i like this method in array {prit}
 
 const all_new_heros = [...marval_heros, ...dc_heros]
 // console.log(all_new_heros);
 
 // other interview qustion
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [8, 9, [4, 5]]]
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
 
 
-console.log(Array.isArray("krish"))
-console.log(Array.from("krish"))
-console.log(Array.from({name: "krish"}))  //intersting interview
+// console.log(Array.isArray("krish"))
+// console.log(Array.from("krish"))
+// console.log(Array.from({name: "krish"}))  //intersting interview
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));      
+// console.log(Array.of(score1, score2, score3));      
+
+// {} <= Object
+// [] <= array
--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,3 +2,48 @@
 
 //  object literals
 
+const mysym = Symbol("key1")
+
+const jsuser = {
+    name: "krish",
+    "full name": "krish gadhiya",
+    [mysym]: "mykey1",
+    age: 18,
+    location: "kagvad",
+    email:"krishgadhiya@gmail.com",
+    isloggdin: false,
+    lastloggedin: ["monday", "saturday"]
+}
+
+// console.log(jsuser.email);
+// console.log(jsuser["email"]);
+// console.log(jsuser["full name"]);
+// console.log(typeof jsuser[mysym]);
+// console.log(jsuser[mysym]);
+
+jsuser.email = "gadhiyakrishgoogle.com"
+// Object.freeze(jsuser)
+jsuser.email = "gadhiyakrishmicrosoft.com"
+// console.log(jsuser);
+
+
+/* symbol methos in object */
+
+// const mysym = Symbol("key1")
+// const jsuser = {
+//     [mysym]: "mykey1"
+// }
+// console.log(jsuser[mysym]);
+
+jsuser.greeting = function(){
+    console.log("hello js user");
+}
+
+jsuser.greetingtwo = function(){
+    console.log(`hello js user, ${this.name}`); 
+}
+
+console.log(jsuser.greeting());
+console.log(jsuser.greetingtwo());
+
+
